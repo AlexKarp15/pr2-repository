f789676442c3e4623366a6515d63dcf5646ba8ab
AlexKarp15 <aleks.karpuk2005@gmail.com>
2024-10-03 19:23:51 +0300

    Додав нові функції для форматування номерів телефонів

--- main.js
+++ main.js
@@ -27,3 +27,31 @@ document.getElementById('countBtn').addEventListener('click', () => {
     
     document.getElementById('result').innerText = resultText;
 });
+
+function formattedPhone(phone) {
+    const phonePattern = /^\+?380\d{9}$|^8\d{10}$|^80\d{9}$|^0\d{9}$/;
+
+    if (!phonePattern.test(phone)) {
+        return 'Неправильний формат телефону';
+    }
+
+    let formatted = '';
+    
+    if (phone.startsWith('+38')) {
+        formatted = phone.replace(/^(\+38)(\d{3})(\d{3})(\d{2})(\d{2})$/, '$1 ($2) $3-$4-$5');
+    } else if (phone.startsWith('8')) {
+        formatted = '+38 ' + phone.replace(/^8(\d{3})(\d{3})(\d{2})(\d{2})$/, '($1) $2-$3-$4');
+    } else if (phone.startsWith('80')) {
+        formatted = '+38 ' + phone.replace(/^80(\d{3})(\d{3})(\d{2})(\d{2})$/, '($1) $2-$3-$4');
+    } else if (phone.startsWith('0')) {
+        formatted = '+38 ' + phone.replace(/^0(\d{3})(\d{3})(\d{2})(\d{2})$/, '($1) $2-$3-$4');
+    }
+
+    return formatted;
+}
+
+document.getElementById('formatBtn').addEventListener('click', () => {
+    const phoneInput = document.getElementById('phoneInput').value;
+    const phoneResult = formattedPhone(phoneInput);
+    document.getElementById('phoneResult').innerText = phoneResult;
+});
